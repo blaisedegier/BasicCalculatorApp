61398b6acce910fdb88d6c6b46be44fb23045854
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-04-26 23:30:22 +0200

    Calculator Class

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    // Delegate for arithmetic operations
+    public delegate void Arithmetic(int num1, int num2);
+    class Calculator
+    {
+        // Event to modify operation performed
+        public event Arithmetic Operation;
+
+        // Methods for arithmetic operations
+        public int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Operation?.Invoke(num1, num2);
+            return result;
+        }
+
+        public int Subtract(int num1, int num2)
+        {
+            int result = num1 - num2;
+            Operation?.Invoke(num1, num2);
+            return result;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            Operation?.Invoke(num1, num2);
+            return result;
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            int result = num1 / num2;
+            Operation?.Invoke(num1, num2);
+            return result;
+        }
+    }
+}
